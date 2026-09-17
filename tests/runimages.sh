2d5a69900cadaf8186ab4662dba2b2a7e9d3923e
MaadhyamRana <864mmr@gmail.com>
2026-09-17 02:58:22 -0400

    examples etc.

---
+++ tests/runimages.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+python cli.py --model vgg19 --octaves 3 --layer features.20 --iterations 100 --image images/logo.jpg --output images/dreamlogo.jpg
+# python cli.py --max-size 800 --model vgg19 --layer features.18 --image images/test0.jpg  --output images/dream0.jpg  
+# python cli.py --max-size 800 --model vgg19 --layer features.21 --image images/test1.jpg  --output images/dream1.jpg  
+# python cli.py --max-size 800 --model vgg19 --layer features.24 --image images/test2.jpg  --output images/dream2.jpg  
+# python cli.py --max-size 800 --model vgg19 --layer features.26 --image images/test3.jpg  --output images/dream3.jpg  
+# python cli.py --max-size 800 --model vgg19 --layer features.27 --image images/test4.jpg  --output images/dream4.jpg
+# python cli.py --max-size 800 --model vgg19 --layer features.28 --image images/test5.jpg  --output images/dream5.jpg
+# python cli.py --max-size 800 --model vgg19 --layer features.29 --image images/test6.jpg  --output images/dream6.jpg
+# python cli.py --max-size 800 --model vgg19 --layer features.30 --image images/test7.jpg  --output images/dream7.jpg
+# python cli.py --max-size 800 --model vgg19 --layer features.31 --image images/test8.jpg  --output images/dream8.jpg
+# python cli.py --max-size 800 --model vgg19 --layer features.32 --image images/test9.jpg  --output images/dream9.jpg
+# python cli.py --max-size 800 --model vgg19 --layer features.33 --image images/test10.jpg --output images/dream10.jpg
+# python cli.py --max-size 800 --model vgg19 --layer features.32 --image images/test11.jpg --output images/dream11.jpg
+# python cli.py --max-size 800 --model vgg19 --layer features.23 --image images/test12.jpg --output images/dream12.jpg
+# python cli.py --max-size 800 --model vgg19 --layer features.26 --image images/test13.jpg --output images/dream13.jpg
+# python cli.py --max-size 800 --model vgg19 --layer features.23 --image images/test16.jpg --output images/dream16.jpg
+# python cli.py --max-size 800 --model vgg19 --layer features.25 --image images/test14.jpg --output images/dream14.jpg
+# python cli.py --max-size 800 --model vgg19 --layer features.28 --image images/test15.jpg --output images/dream15.jpg
